5eb13c95b7fc457d8b3b359b151b1e1ff4c50558
mjtordesillas <you@example.com>
2019-06-13 07:57:32 +0200

    tested that for normal items, quality degrades by one before sell by date

--- test/gilded_rose/core_test.clj
+++ test/gilded_rose/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [gilded-rose.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest gilded-rose-test
+  (testing "For normal items, quality degrades by one before sell by date"
+    (let [normal-item {:name "+5 Dexterity Vest" :sell-in 5 :quality 5}
+          updated-item {:name "+5 Dexterity Vest" :sell-in 4 :quality 4}]
+      (is (= [updated-item] (update-quality [normal-item]))))))
