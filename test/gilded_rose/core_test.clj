ccf30ca4e4ba7f01376048bba3b2fa016c5bb8f0
mjtordesillas <you@example.com>
2019-06-14 08:42:00 +0200

    refactor: made tests more expressive, using item function for item creation

--- test/gilded_rose/core_test.clj
+++ test/gilded_rose/core_test.clj
@@ -4,13 +4,13 @@
 
 (deftest gilded-rose-test
   (testing "For normal items, quality degrades by one before sell by date"
-    (let [normal-item {:name "+5 Dexterity Vest" :sell-in 5 :quality 5}
+    (let [normal-item (item "+5 Dexterity Vest" 5 5)
           updated-quality 4]
       (is (= updated-quality (:quality (first (update-quality [normal-item])))))))
   (testing "For all items except Sulfuras, sell in date decreases by one"
-    (let [any-item {:name "any item" :sell-in 20 :quality 30}
+    (let [not-sulfuras (item "not sulfuras" 20 30)
           updated-sell-in 19]
-      (is (= updated-sell-in (:sell-in (first (update-quality [any-item])))))))
+      (is (= updated-sell-in (:sell-in (first (update-quality [not-sulfuras])))))))
   (testing "Aged Brie increases in quality the older it gets"
     (let [aged-brie (item "Aged Brie" 5 5)
           updated-quality 6]
