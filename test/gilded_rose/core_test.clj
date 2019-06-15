ec567cc9b4b7b11a99744e9ac144d3cb60f0abdd
mjtordesillas <you@example.com>
2019-06-15 06:46:37 +0200

    testing that normal items quality degrades by two after the sell in date

--- test/gilded_rose/core_test.clj
+++ test/gilded_rose/core_test.clj
@@ -7,6 +7,10 @@
     (let [normal-item (item "+5 Dexterity Vest" 5 5)
           updated-quality 4]
       (is (= updated-quality (:quality (first (update-quality [normal-item])))))))
+  (testing "For normal items, quality degrades by two after sell by date"
+    (let [normal-item (item "+5 Dexterity Vest" 0 5)
+          updated-quality 3]
+      (is (= updated-quality (:quality (first (update-quality [normal-item])))))))
   (testing "For all items except Sulfuras, sell in date decreases by one"
     (let [not-sulfuras (item "not sulfuras" 20 30)
           updated-sell-in 19]
