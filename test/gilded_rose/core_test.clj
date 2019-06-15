0a2ae8c1867e155ba2df86b8e0502f08b04e7867
mjtordesillas <you@example.com>
2019-06-15 06:49:09 +0200

    refactor: changed tests order

--- test/gilded_rose/core_test.clj
+++ test/gilded_rose/core_test.clj
@@ -3,6 +3,14 @@
             [gilded-rose.core :refer :all]))
 
 (deftest gilded-rose-test
+  (testing "For all items except Sulfuras, sell in date decreases by one"
+    (let [not-sulfuras (item "not sulfuras" 20 30)
+          updated-sell-in 19]
+      (is (= updated-sell-in (:sell-in (first (update-quality [not-sulfuras])))))))
+  (testing "Sulfuras never ages"
+    (let [sulfuras (item "Sulfuras, Hand of Ragnaros" 5 5)
+          updated-sell-in 5]
+      (is (= updated-sell-in (:sell-in (first (update-quality [sulfuras])))))))
   (testing "For normal items, quality degrades by one before sell by date"
     (let [normal-item (item "+5 Dexterity Vest" 5 5)
           updated-quality 4]
@@ -11,10 +19,6 @@
     (let [normal-item (item "+5 Dexterity Vest" 0 5)
           updated-quality 3]
       (is (= updated-quality (:quality (first (update-quality [normal-item])))))))
-  (testing "For all items except Sulfuras, sell in date decreases by one"
-    (let [not-sulfuras (item "not sulfuras" 20 30)
-          updated-sell-in 19]
-      (is (= updated-sell-in (:sell-in (first (update-quality [not-sulfuras])))))))
   (testing "Aged Brie increases in quality the older it gets"
     (let [aged-brie (item "Aged Brie" 5 5)
           updated-quality 6]
@@ -23,10 +27,6 @@
     (let [sulfuras (item "Sulfuras, Hand of Ragnaros" 5 5)
           updated-quality 5]
       (is (= updated-quality (:quality (first (update-quality [sulfuras])))))))
-  (testing "Sulfuras never ages"
-    (let [sulfuras (item "Sulfuras, Hand of Ragnaros" 5 5)
-          updated-sell-in 5]
-      (is (= updated-sell-in (:sell-in (first (update-quality [sulfuras])))))))
   (testing "The quality of an item is never more than 50"
     (let [aged-brie (item "Aged Brie" 5 50)
           updated-quality 50]
