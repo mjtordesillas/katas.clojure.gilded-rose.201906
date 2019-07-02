2fe0f0d07f4a260cfe10a44b9f6e5be5b62b2653
mjtordesillas <you@example.com>
2019-07-02 07:27:21 +0200

    refactor: aging items inside of update function

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -7,60 +7,59 @@
 (defn- age-one-day [item]
   (merge item {:sell-in (dec (:sell-in item))}))
 
-(defmulti update-item-quality
+(defmulti update-item
           (fn [item] (:name item)))
 
-(defmethod update-item-quality "Backstage passes to a TAFKAL80ETC concert" [item]
-  (if (< (:sell-in item) 0)
-    (merge item {:quality 0})
-    (if (and (>= (:sell-in item) 5) (< (:sell-in item) 10) (< (:quality item) 50))
+(defmethod update-item "Backstage passes to a TAFKAL80ETC concert" [item]
+  (let [item (age-one-day item)]
+    (if (< (:sell-in item) 0)
+      (merge item {:quality 0})
+      (if (and (>= (:sell-in item) 5) (< (:sell-in item) 10) (< (:quality item) 50))
+        (if (<= (:quality item) 48)
+          (update-item-quality-by item 2)
+          (update-item-quality-by item 1))
+        (if (and (>= (:sell-in item) 0) (< (:sell-in item) 5) (< (:quality item) 50))
+          (if (<= (:quality item) 47)
+            (update-item-quality-by item 3)
+            (if (<= (:quality item) 48)
+              (update-item-quality-by item 2)
+              (update-item-quality-by item 1)))
+          (if (< (:quality item) 50)
+            (merge item {:quality (inc (:quality item))})
+            item))))))
+
+(defmethod update-item "Aged Brie" [item]
+  (let [item (age-one-day item)]
+    (if (and (< (:sell-in item) 0) (< (:quality item) 50))
       (if (<= (:quality item) 48)
         (update-item-quality-by item 2)
         (update-item-quality-by item 1))
-      (if (and (>= (:sell-in item) 0) (< (:sell-in item) 5) (< (:quality item) 50))
-        (if (<= (:quality item) 47)
-          (update-item-quality-by item 3)
-          (if (<= (:quality item) 48)
-            (update-item-quality-by item 2)
-            (update-item-quality-by item 1)))
-        (if (< (:quality item) 50)
-          (merge item {:quality (inc (:quality item))})
-          item)))))
+      (if (< (:quality item) 50)
+        (update-item-quality-by item 1)
+        item))))
 
-(defmethod update-item-quality "Aged Brie" [item]
-  (if (and (< (:sell-in item) 0) (< (:quality item) 50))
-    (if (<= (:quality item) 48)
-      (update-item-quality-by item 2)
-      (update-item-quality-by item 1))
-    (if (< (:quality item) 50)
-      (update-item-quality-by item 1)
-      item)))
+(defmethod update-item :default [item]
+  (let [item (age-one-day item)]
+    (if (< (:sell-in item) 0)
+      (if (> (:quality item) 1)
+        (update-item-quality-by item -2)
+        (merge item {:quality 0}))
+      (if (> (:quality item) 0)
+        (update-item-quality-by item -1)
+        item))))
 
-(defmethod update-item-quality :default [item]
-  (if (< (:sell-in item) 0)
-    (if (> (:quality item) 1)
-      (update-item-quality-by item -2)
-      (merge item {:quality 0}))
-    (if (> (:quality item) 0)
-      (update-item-quality-by item -1)
-      item)))
-
-(defmethod update-item-quality "Sulfuras, Hand of Ragnaros" [item]
+(defmethod update-item "Sulfuras, Hand of Ragnaros" [item]
   (merge item {:quality 80}))
 
 (defn update-quality [items]
   (map
-    (fn[item] (update-item-quality item))
-    (map (fn [item]
-           (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-             (age-one-day item)
-             item))
-         items)))
+    (fn [item] (update-item item))
+    items))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
 
-(defn update-current-inventory[]
+(defn update-current-inventory []
   (let [inventory
         [
          (item "+5 Dexterity Vest" 10 20)
