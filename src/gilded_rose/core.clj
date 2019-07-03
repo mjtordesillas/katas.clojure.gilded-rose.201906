0a7e903cb6b9b039cc2912e7ff9fba517508d748
mjtordesillas <you@example.com>
2019-07-03 07:11:26 +0200

    refactor: created multimethod for aging items, calling it from outside

--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -14,40 +14,43 @@
       :else
       (merge item {:quality updated-quality}))))
 
-(defn- age-one-day [item]
+(defmulti age-item
+          (fn [item] (:name item)))
+
+(defmethod age-item "Sulfuras, Hand of Ragnaros" [item]
+  item)
+
+(defmethod age-item :default [item]
   (merge item {:sell-in (dec (:sell-in item))}))
 
 (defmulti update-item
           (fn [item] (:name item)))
 
 (defmethod update-item "Backstage passes to a TAFKAL80ETC concert" [item]
-  (let [item (age-one-day item)]
-    (if (< (:sell-in item) 0)
-      (merge item {:quality 0})
-      (if (and (>= (:sell-in item) 5) (< (:sell-in item) 10))
-        (update-item-quality-by item 2)
-        (if (and (>= (:sell-in item) 0) (< (:sell-in item) 5))
-          (update-item-quality-by item 3)
-          (update-item-quality-by item 1))))))
+  (if (< (:sell-in item) 0)
+    (merge item {:quality 0})
+    (if (and (>= (:sell-in item) 5) (< (:sell-in item) 10))
+      (update-item-quality-by item 2)
+      (if (and (>= (:sell-in item) 0) (< (:sell-in item) 5))
+        (update-item-quality-by item 3)
+        (update-item-quality-by item 1)))))
 
 (defmethod update-item "Aged Brie" [item]
-  (let [item (age-one-day item)]
-    (if (< (:sell-in item) 0)
-      (update-item-quality-by item 2)
-      (update-item-quality-by item 1))))
+  (if (< (:sell-in item) 0)
+    (update-item-quality-by item 2)
+    (update-item-quality-by item 1)))
 
 (defmethod update-item :default [item]
-  (let [item (age-one-day item)]
-    (if (< (:sell-in item) 0)
-      (update-item-quality-by item -2)
-      (update-item-quality-by item -1))))
+  (if (< (:sell-in item) 0)
+    (update-item-quality-by item -2)
+    (update-item-quality-by item -1)))
 
 (defmethod update-item "Sulfuras, Hand of Ragnaros" [item]
   (merge item {:quality 80}))
 
 (defn update-quality [items]
   (map
-    (fn [item] (update-item item))
+    (fn [item] (update-item (age-item item)))
     items))
 
 (defn item [item-name, sell-in, quality]
